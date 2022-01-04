ddda64e82f8d73e718e5378399470693b79f92ee
aryamanbgupta <85473680+aryamanbgupta@users.noreply.github.com>
2022-01-04 11:28:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const socket = io('/')
 let IsConnectedtoCustomer= false
 //do we need two html? one for seller and one for buyer?
-if(!IsConnectedtoCustomer){
-  //take the next one from the queue
-}
+//Check if buyer is present?
 
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
   port: '3001'
 })
+
+let CustomerQueue=[]
+if(!IsConnectedtoCustomer){
+  //take the next one from the queue 
+  if(CustomerQueue[0]!=null){
+    //make the userid at postion 1 join the room
+    let newUserID= CustomerQueue[0]
+    //make newUserID join the connection? need to use server.js?
+    //redirect from waiting room to the meeting
+  }
+}
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
@@ -17,30 +26,45 @@ navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  addVideoStream(myVideo, stream)
-
+  if(!IsConnectedtoCustomer){
+    addVideoStream(myVideo, stream)
+  }
   myPeer.on('call', call => {
-    console.log(IsConnectedtoCustomer)
-    call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
+    //console.log(IsConnectedtoCustomer)
+    if(!IsConnectedtoCustomer){
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+      })
+      IsConnectedtoCustomer=true
+    }
     
   })
 
   socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
-    
+    //if(!IsConnectedtoCustomer){
+     // IsConnectedtoCustomer=true
+      connectToNewUser(userId, stream)
+    //}
   })
 })
 
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
+  IsConnectedtoCustomer=false
+  socket.emit('get-waiting-user')
 })
 
 myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
+  if(!IsConnectedtoCustomer)
+    socket.emit('join-room', ROOM_ID, id)
+  else {
+    let i=0
+    while(CustomerQueue[i]!=null) i++
+    CustomerQueue[i]=id
+    socket.emit('join-waiting',ROOM_ID,id)
+  }
 })
 
 function connectToNewUser(userId, stream) {
@@ -49,17 +73,19 @@ function connectToNewUser(userId, stream) {
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
       addVideoStream(video, userVideoStream)
+      document.getElementById("DispUser").innerHTML=userId
       IsConnectedtoCustomer=true
-      console.log(IsConnectedtoCustomer)
+      //console.log(IsConnectedtoCustomer)
     })
     call.on('close', () => {
       video.remove()
-      IsConnectedtoCustomer=false
-      console.log(IsConnectedtoCustomer)
+      //IsConnectedtoCustomer=false
+      //console.log(IsConnectedtoCustomer)
 
     })
 
-    peers[userId] = call}
+    peers[userId] = call
+  }
   else{
     //join waiting room
     showWaitingRoom()
@@ -73,6 +99,7 @@ function addVideoStream(video, stream) {
   })
   videoGrid.append(video)
 }
+
 
 //create new bool IsConnectedtoCustomer
 //if it is false then peers can join else they are put into a waiting room and added to a queue/array/vector
